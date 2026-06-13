c60b72b2137b9c2656e58bc2f8e4f014e130c38f
1537035648@qq.com <1537035648@qq.com>
2026-06-14 05:49:59 +0800

    fix: 移除头像溢出 -mt-9，头像在大气泡内居中排列

--- src/components/RightSidebar.vue
+++ src/components/RightSidebar.vue
@@ -80,7 +80,7 @@ const lastUpdated = computed(() => {
     <div v-else>
       <KunCard color="background" bordered class="p-5 flex flex-col items-center">
         <!-- 正方形头像小气泡（略小于父级宽度，10% 圆角，头像填满气泡） -->
-        <div class="w-5/6 max-w-[180px] aspect-square rounded-lg overflow-hidden border-2 border-default-200 -mt-9 mb-3 bg-default-200">
+        <div class="w-5/6 max-w-[180px] aspect-square rounded-lg overflow-hidden border-2 border-default-200 mb-3 bg-default-200">
           <img :src="AVATAR_URL" alt="撰文人" class="w-full h-full object-cover" />
         </div>
         <!-- 居中名称 -->
